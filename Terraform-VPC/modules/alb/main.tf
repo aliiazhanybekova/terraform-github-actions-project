f56f7fd2ff85b6b033ab99785369a4afce6150d5
aliiazhanybekova <alex.zhanybekova@gmail.com>
2025-01-14 16:42:13 -0500

    new

--- Terraform-VPC/modules/alb/main.tf
+++ Terraform-VPC/modules/alb/main.tf
@@ -23,7 +23,7 @@ resource "aws_lb_listener" "listener" {
 resource "aws_lb_target_group" "tg" {
   name     = "tg"
   port     = 80
-  protocol = "HTTP"
+  protocol = "HTTPS"
   vpc_id   = var.vpc_id
 }
 
